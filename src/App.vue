cfb690810a0002a0387ddac0fbc90e3c404e0163
JustineGroiset <justine.groiset@outlook.fr>
2023-05-21 16:21:37 +0200

    Add style

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,17 @@
 
 <template>
   <h1>VueJs - training Justine Groiset</h1>
+
+  <main>
+    <section id="mainPart">
+
+      <p>Test</p>
+
+    </section>
+    <section id="secondPart">
+
+    </section>
+  </main>
 </template>
 
 <style scoped>
